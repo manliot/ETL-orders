300ce1f8528af0d916a871c6afa4d52a3089f25d
Manlio Tejeda <manliot@uninorte.edu.co>
2024-05-10 19:44:17 -0500

    refactor: change table column names

--- dags/queries/ddl_airflow_etl_db.sql
+++ dags/queries/ddl_airflow_etl_db.sql
@@ -3,13 +3,10 @@
 -- DROP DATABASE IF EXISTS airflow_etl;
 
 
-CREATE DATABASE IF NOT EXISTS airflow_etl
+CREATE DATABASE airflow_etl
     WITH
     OWNER = postgres
     ENCODING = 'UTF8'
-    LC_COLLATE = 'English_United States.1252'
-    LC_CTYPE = 'English_United States.1252'
-    LOCALE_PROVIDER = 'libc'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1
     IS_TEMPLATE = False; 
